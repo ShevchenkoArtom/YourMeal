e4ee35fd876af034ead63baefd1c44a430444e26
Artom Shevchenko <87131438+ShevchenkoArtom@users.noreply.github.com>
2023-07-25 18:08:35 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,147 @@
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index];
+        popupLink.addEventListener("click", function (e) {
+            const popupName = popupLink.getAttribute('href').replace('#', '');
+            const curentPopup = document.getElementById(popupName);
+            popupOpen(curentPopup);
+            e.preventDefault();
+        });
+    }
+}
+const popupCloseIcon = document.querySelectorAll('.close__popup');
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupClose(el.closest(".popup"));
+            e.preventDefault();
+        });
+    }
+}
+
+function popupOpen(curentPopup) {
+    if (curentPopup && unlock) {
+        const popupActive = document.querySelector('.popup.open');
+        if (popupActive) {
+            popupClose(popupActive, false);
+        } else {
+            bodyLock();
+        }
+        curentPopup.classList.add('open');
+        curentPopup.addEventListener("click", function (e) {
+            if (!e.target.closest('.popup__content')) {
+                popupClose(e.target.closest(".popup"));
+            }
+        });
+    }
+}
+function popupClose (popupActive, doUnlock = true) {
+    if (unlock) {
+        popupActive.classList.remove("open");
+        if (doUnlock) {
+            bodyUnLock();
+        }
+    }
+}
+
+function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+    
+    if (lockPadding.length > 0) {
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
+        }
+    }
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout);
+}
+
+function bodyUnLock() {
+    setTimeout(function () {
+        if (lockPadding.length > 0) {
+            for (let index; index < lockPadding.length; index++) {
+                const el = lockPadding[index];
+                el.style.paddingRight = "0px";
+            }
+        }
+        body.style.paddingRight = "0px";
+        body.classList.remove('lock');
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function (){
+        unlock = true;
+    }, timeout);
+}
+
+document.addEventListener('keydown', function (e){
+    if (e.which === 27) {
+        const popupActive = document.querySelector(".popup.open");
+        popupClose(popupActive);
+    }
+});
+
+// ТАБЫ ТАБЫ ТАБЫ ТАБЫ ТАБЫ ТАБЫ ТАБЫ ТАБЫ
+
+let tab = function () {
+    let tabNav = document.querySelectorAll(".assortment__list-item"),
+        tabContent = document.querySelectorAll(".tab"),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    })
+
+    function selectTabNav(){
+        tabNav.forEach(item=> {
+            item.classList.remove('is-active');
+        })
+        this.classList.add('is-active');
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('is-active'): item.classList.remove('is-active');
+        })
+    }
+};
+
+tab();
+
+(function () {
+    if (!Element.prototype.closest) {
+        Element.prototype.closest = function (css) {
+            var node = this;
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
+    }
+})();
+(function () {
+    if (!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector;
+    }
+})();
